8f63ba155e5f1c063e838ba5d975970cb6c45ce8
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-07-08 20:46:18 -0500

    cambios antes de borrar secciones grandes

---
+++ client/app/myhome.js
@@ -0,0 +1,35 @@
+
+
+Vue.component('myhome', {
+    template:
+    `
+    <div>
+
+        <section id="home-section" class="hero">
+            <div class="home-slider owl-carousel">
+            <div class="slider-item" style="background-image: url(images/bg_1.jpg);">
+                <div class="overlay"></div>
+                <div class="container">
+                    <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
+                        <div class="col-md-12  text-center">
+                            <h1 class="mb-2">We serve Fresh Vegestables &amp; Fruits</h1>
+                            <h2 class="subheading mb-4">We deliver organic vegetables &amp; fruits</h2>
+                            <p><a href="#" class="btn btn-primary">View Details</a></p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        
+
+
+    </div>
+    `,
+    data() {
+        return {
+
+        }
+    }
+})
