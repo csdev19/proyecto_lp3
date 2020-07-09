8a08322de9e1ce8024015b5193be2faee10f1ffe
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-07-08 21:38:45 -0500

    cambios y un cliente que no hace nada que luego borro

--- client/app/myhome.js
+++ client/app/myhome.js
@@ -6,10 +6,10 @@ Vue.component('myhome', {
     <div>
 
         <section id="home-section" class="hero">
-            <div class="home-slider owl-carousel">
+            <div class="home-slider estilo_portada">
             <div class="slider-item" style="background-image: url(images/bg_1.jpg);">
                 <div class="overlay"></div>
-                <div class="container">
+                <div class="container texto-class">
                     <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
                         <div class="col-md-12  text-center">
                             <h1 class="mb-2">We serve Fresh Vegestables &amp; Fruits</h1>
